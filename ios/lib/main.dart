2b4e300df32055b69f443d5013b61aa1c3eac0b0
antoniolins <antonioqueirozlins@gmail.com>
2025-05-04 14:20:48 -0300

    Author in Main File

--- ios/lib/main.dart
+++ ios/lib/main.dart
@@ -1,9 +1,17 @@
+
+//***
+// by Antonio Lins - 04/05/2025
+// an MVVM Flutter project WITH GETIT
+// 
+// */
 import 'package:flutter/material.dart';
 import 'app/data/services/local_service_db/local_task_service_db.dart';
 import 'app/app_widget.dart';
 import 'app/core/service_locator.dart';
 
 late LocalTaskServiceDB localTaskServiceDB;
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
